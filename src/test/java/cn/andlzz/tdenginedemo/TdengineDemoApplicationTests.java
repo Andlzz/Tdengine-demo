03fbcf7d97f576602b9ab49e65d0dc5dbc1faa55
杜亮志 <1977607896@foxmail.com>
2024-05-16 11:33:03 +0800

    时间查询

--- src/test/java/cn/andlzz/tdenginedemo/TdengineDemoApplicationTests.java
+++ src/test/java/cn/andlzz/tdenginedemo/TdengineDemoApplicationTests.java
@@ -7,6 +7,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Slf4j
@@ -41,10 +46,24 @@ class TdengineDemoApplicationTests {
         EquipmentInfo equipmentInfo = new EquipmentInfo();
         equipmentInfo.setEqpId("1977607896");
         equipmentInfo.setTableName("eqp_1977607896");
+        LocalDateTime startTime = LocalDateTime.of(2024, 4, 26, 16, 58, 42);
+        ZoneId zoneId = ZoneId.of("Asia/Shanghai");
+        ZonedDateTime zonedDateTime = startTime.atZone(zoneId);
+        Instant instant = zonedDateTime.toInstant();
+        long timestamp = instant.toEpochMilli();
+        // 格式化时间为yyyy-MM-dd HH:mm:ss
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String format = startTime.format(formatter);
         List<EquipmentInfo> list = equipmentInfoService.lambdaQuery()
+                .ge(EquipmentInfo::getTs, timestamp)
+                .eq(EquipmentInfo::getEqpId, equipmentInfo.getEqpId())
+                .list();
+        List<EquipmentInfo> list1 = equipmentInfoService.lambdaQuery()
+                .ge(EquipmentInfo::getTs, format)
                 .eq(EquipmentInfo::getEqpId, equipmentInfo.getEqpId())
                 .list();
         log.info("查询结果：{}", list);
+        log.info("查询结果：{}", list1);
     }
 
 }
